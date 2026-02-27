1f23db34288b41a7cb2bc0bd2aa31c418ce25f3e
Kevin Aimaier <kflyn825@outlook.com>
2026-02-28 01:31:03 +0800

    minor fix in test/test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # test.sh
 
-JSON_CFG="example.json"
+JSON_CFG="image.json"
 CMD=""
 options=""
 
 # USE $JSON_CFG TO PASS "image.json"
-CMD=$(../resolver.sh "$JSON_CFG" "build.cmd")
-#options=$(./resolver.sh "$JSON_CFG" "build.options[]")
 
-echo "Resolved CMD: $CMD"
+#options=$(../resolver.sh "$JSON_CFG" "build.options[]")
+#echo -e "Resolved options: \n$options" | sed -r 's/[[:space:]]{4,}/    \n  /g'
+
+CMD=$(../resolver.sh "$JSON_CFG" "build.cmd")
+echo -e "Resolved CMD: \n$CMD" | sed -r 's/[[:space:]]{4,}/    \n  /g'
+
 #echo "Resulting Options: $options"
